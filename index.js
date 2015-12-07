805ca2c399d6e59771827a36263c7202904f645a
Jim Bulkowski <jim.b@paperelectron.com>
2015-12-07 00:29:14 -0500

    Actually do something usefull

--- index.js
+++ index.js
@@ -5,8 +5,6 @@
  * @license MIT {@link http://opensource.org/licenses/MIT}
  */
 
-var request = require('request');
-
 /**
  * Provides Controllers for the Pomegranate framework.
  * @module pomegranate-example-plugin
@@ -15,18 +13,50 @@ var request = require('request');
 module.exports = {
 
   /**
-   *
+   * The main hook for your plugin, whatever is passed to the callback will be loaded into the injector.
    * @param {Function} inject The Magnum DI injector
    * @param {Function} loaded Callback function to return your plugins main object to the injector.
    */
   load: function(inject, loaded) {
-    request('https://www.reddit.com/r/node.json', function(err, res, body){
-      loaded(err, body)
-    })
+    /*
+     * "this" has a few interesting properties you can use.
+     *  this.options contains the options (if any) for this plugin.
+     *  this.Logger Is the global Logger in use by the app. Best to use this for any output
+     *  to help respect your users wishes.
+     */
+    var self = this;
+    this.Logger.log(self);
+
+    /*
+     * We need to call our loaded function with an object when we are all done doing what we need it to do.
+     */
+    var myPluginObject = {
+      name: 'Example',
+      sayName: function(){
+        self.Logger.log(myPluginObject.name)
+      }
+    };
+    /*
+     * Async is ok obviously, though there is a 2000ms timeout by default on all of the hooks
+     * exposed.
+     */
+    setTimeout(function(){
+      self.Logger.log('Output here should only include critical info.')
+      loaded(null, myPluginObject)
+    }, 1000)
   },
+  /**
+   * Startup hook, called when the application is done loading, any startup tasks for you plugin go here.
+   * @param {Function} done
+   */
   start: function(done) {
     done(null)
   },
+
+  /**
+   * Stop hook, called before the application is shut down. Cleanup tasks for your plugin go here.
+   * @param {Function} done
+   */
   stop: function(done) {
     done(null)
   }
