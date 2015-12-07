e4dd8f7cc2caae1fa650c1e863cb61258fdd0296
Jim Bulkowski <jim.b@paperelectron.com>
2015-12-07 00:03:50 -0500

    Added some data to be returned

--- index.js
+++ index.js
@@ -5,6 +5,8 @@
  * @license MIT {@link http://opensource.org/licenses/MIT}
  */
 
+var request = require('request');
+
 /**
  * Provides Controllers for the Pomegranate framework.
  * @module pomegranate-example-plugin
@@ -18,7 +20,9 @@ module.exports = {
    * @param {Function} loaded Callback function to return your plugins main object to the injector.
    */
   load: function(inject, loaded) {
-
+    request('https://www.reddit.com/r/node.json', function(err, res, body){
+      loaded(err, body)
+    })
   },
   start: function(done) {
     done(null)
